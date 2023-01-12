eb841ec1392f337eee3a24b7aa4105b611950def
joemeetjoe <joemeetjoe@gmail.com>
2023-01-11 22:06:12 -0500

    added in the function to populate the 1 day section of the site along with the emoji for the weather, wind speed, temp, humidity; added in comments and readme.

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,20 @@
 // adding the search button as a global variable to listen to later
-var searchButton = document.getElementById("search-button");
+let searchButton = document.getElementById("search-button");
 let searchCriteria = document.getElementById("search-criteria");
 let bigContainer = document.getElementById("one-day-5-day-container");
 let historyButtonContainers = document.getElementById("city-button-list");
+let cityNameHeader = document.getElementById("city-name-header");
+let oneDayTemp = document.getElementById("one-day-temp");
+let oneDayWind = document.getElementById("one-day-wind");
+let oneDayHumidity = document.getElementById("one-day-humidity");
+
 // setting the 5 day containers and city containers to invisible
 bigContainer.style.display = "none";
-
-
-
 // function passing in the searchcritera.value from the function init
 function getLongAndLat(a){
+  // setting the city name in the city name header
+  console.log(a);
+  cityNameHeader.textContent = a;
   // constructing the api call and plugging in the city 
     var LatLonApiCall = "http://api.openweathermap.org/geo/1.0/direct?q=" + a + "&limit=1&appid=45243bff2680411dc8b71cd9714f866f";
     // fetch call plugging in the api call and parsing it. 
@@ -25,7 +30,7 @@ function getLongAndLat(a){
     })
 };
 
-
+// setting an empty variable called weather 5 day data
 let weather5DayData;
 // 5 day weather function plugging in the lat and long from function getLatandLong
 function getWeather5Day(a, b){
@@ -40,14 +45,14 @@ function getWeather5Day(a, b){
       
       // getting the date and time, first we set the loop equal to the array inside the object
       weather5DayData = data.list;
-      console.log(weather5DayData);
+      // adding in empty variables to use as empty arrays
       var weather5DayDates = [];
       var weather5DayTemps = [];
       var weather5DayWinds = [];
       var weather5DayHumids = [];
       var emojiWeatherArray = [];
       var convertedEmojiArray = [];
-      
+      var weather1DayEmoji;
       // for each object in the array, loop through (skipping 8 spots each time to equal 5 day forecast)
       for (var i = 0; i < data.list.length; i+=8){
         // grab the date out of the list, convert it from unix code to normal days, then the MM/DD/yyyy format
@@ -68,12 +73,12 @@ function getWeather5Day(a, b){
         var humidity = data.list[i].main.humidity;
         humidity = "Humidity: " + humidity + "%";
         weather5DayHumids.push(humidity);
-      
-      
+        
         // adding in emojis depending on the weather
         var emojiWeather = data.list[i].weather[0].main;
         emojiWeatherArray.push(emojiWeather);
       }; 
+
         // looping through the saved weather statuses and converting them to emojis
         for ( var j = 0; j < emojiWeatherArray.length; j++){
           if (emojiWeatherArray[j] === "Clouds"){
@@ -86,7 +91,7 @@ function getWeather5Day(a, b){
             convertedEmojiArray.push("Snow ❄️");
           };
         };
-      
+
       // get all of the cards that belong to the five day card class name, to loop through
      let fiveDayCardClass = document.getElementsByClassName("five-day-card");
       //  fiveDayCardClass.style.display = "block";
@@ -102,7 +107,7 @@ function getWeather5Day(a, b){
       // adding a line break
       var breaker2 = document.createElement("br");
       fiveDayCardClass[i].appendChild(breaker2);
-      // adding in the temps
+      // adding in the temps to the five day
       var weather5DayTempsPlacer = document.createTextNode(weather5DayTemps[i]);
       fiveDayCardClass[i].appendChild(weather5DayTempsPlacer);
       // adding a line break
@@ -121,12 +126,39 @@ function getWeather5Day(a, b){
       // setting the city container and 5 day cards to visible
       bigContainer.style.display = "block";
 
-
-      // add 1 day forecast here.
+      // adding in the temps to the 1 day
+      var weather1DayTempsPlacer = document.createTextNode(weather5DayTemps[0]);
+      oneDayTemp.appendChild(weather1DayTempsPlacer);
+      // adding in the wind to the 1 day
+      var weather1DayWindPlacer = document.createTextNode(weather5DayWinds[0]);
+      oneDayWind.appendChild(weather1DayWindPlacer);
+      // adding in the humidity to the 1 day
+      var weather1DayHumidsPlacer = document.createTextNode(weather5DayHumids[0]);
+      oneDayHumidity.appendChild(weather1DayHumidsPlacer);
+      // adding in the current date to the 1 day
+      var currentDayHeader = document.createTextNode(" " + dayjs().format("MM/DD/YYYY"));
+      cityNameHeader.appendChild(currentDayHeader);
+      
+      // setting the emoji for the header section by checking the first position of the weather data
+      weather1DayEmoji = data.list[0].weather[0].main;
+        if (weather1DayEmoji === "Clouds"){
+          var emojiPlacer = document.createTextNode(" ☁️");
+          cityNameHeader.appendChild(emojiPlacer);
+        } else if (weather1DayEmoji == "Rain"){
+          var emojiPlacer = document.createTextNode(" 🌨️");
+          cityNameHeader.appendChild(emojiPlacer);
+        } else if (weather1DayEmoji === "Clear"){
+          var emojiPlacer = document.createTextNode(" ☀️");
+          cityNameHeader.appendChild(emojiPlacer);
+        } else if (weather1DayEmoji === "Snow"){
+          var emojiPlacer = document.createTextNode(" ❄️");
+          cityNameHeader.appendChild(emojiPlacer);
+        };
     });
 };
-
+// setting an empty array for the search history to be put into
 var searchHistoryArray = [];
+// creating a function to add the search history buttons and save searches to local storage
 function addSearchHistoryButtons(a){
   // creating buttons with the text content that is added inside of the search box
   var historyButtons =document.createElement("button");
@@ -138,7 +170,7 @@ function addSearchHistoryButtons(a){
   console.log(searchHistoryArray);
   localStorage.setItem( "past searches", JSON.stringify(searchHistoryArray));
 };
-console.log(searchHistoryArray);
+
 // localStorage.setItem( "past searches", JSON.stringify(searchHistoryArray));
 // pulling history from local storage and setting them as buttons
 var retrievedHistory = localStorage.getItem("past searches");
@@ -159,12 +191,15 @@ function historyInit(event){
 searchButton.addEventListener("click", init);
 
 // for the amount of items in the retrieved search history, add a button with the text content
-for ( i = 0; i < retrievedHistory.length; i++){
-  var historyButtons =document.createElement("button");
-  historyButtons.textContent = retrievedHistory[i];
-  historyButtons.classList.add("history-buttons");
-  historyButtonContainers.appendChild(historyButtons);
-};
+if ( retrievedHistory != null){
+  for ( i = 0; i < retrievedHistory.length; i++){
+    var historyButtons =document.createElement("button");
+    historyButtons.textContent = retrievedHistory[i];
+    historyButtons.classList.add("history-buttons");
+    historyButtonContainers.appendChild(historyButtons);
+  };
+}
+
 // adding an event listener to the history buttons section
 historyButtonContainers.addEventListener("click", historyInit);
 
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # weather-dashboard
+
+## Description
+
+-A web app to help you search for weather conditions. 
+
+- I took this project on to learn how to use the open weather map API . 
+- This site will help a user look up the weather in different cities around the world. When the user searches for a city, the user is presented with a 5 day forecast, a current day forecast, along with wind speeds, temperatures, and humidity. The search is then stored into local storage and a search history button for the user to be able to quickly navigate to later. 
+- I learned many things on this project: How to use Open Weather API, more about bootstrap, how to read documentation for an API, how to navigate the data in an API, more about functions and controlling order flow.   
+-If given more time on this project, I would like to do more to clean up user input, make the data responses more robust, and do more with the styling and responsiveness of the site. 
+
+## Installation
+
+N/A
+
+## Usage
+
+If you would like to use this project, please click on the link below: 
+
+## License
+
+There is no license on this repository. 
+
+##credits
+
+Day.js, Open Weather API.
